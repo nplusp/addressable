97411afc4bb758572cebb0969af2990af6827041
Nick Pridorozhko <nick.pridorozhko@gmail.com>
2015-10-01 19:50:43 +0300

    use interpolation instead of concatenation
    Interpolation works faster than concatenation and more "Ruby-way"

--- lib/addressable/template.rb
+++ lib/addressable/template.rb
@@ -25,14 +25,12 @@ module Addressable
   # RFC 6570 (http://tools.ietf.org/html/rfc6570).
   class Template
     # Constants used throughout the template code.
-    anything =
-      Addressable::URI::CharacterClasses::RESERVED +
-      Addressable::URI::CharacterClasses::UNRESERVED
+    anything = 
+      "#{Addressable::URI::CharacterClasses::RESERVED}#{Addressable::URI::CharacterClasses::UNRESERVED}"
 
 
     variable_char_class =
-      Addressable::URI::CharacterClasses::ALPHA +
-      Addressable::URI::CharacterClasses::DIGIT + '_'
+      "#{Addressable::URI::CharacterClasses::ALPHA}#{Addressable::URI::CharacterClasses::DIGIT}_"
 
     var_char =
       "(?:(?:[#{variable_char_class}]|%[a-fA-F0-9][a-fA-F0-9])+)"
